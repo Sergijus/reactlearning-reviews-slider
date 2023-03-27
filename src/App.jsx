025b5f0319152fa0e425fc61c7996294cca5d307
Sergijus <web.srdjan.marinkovic@gmail.com>
2023-03-27 21:34:17 +0200

    Reviews app with arrows and random

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,67 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import data from './data';
+import { FaChevronRight, FaChevronLeft, FaQuoteRight } from 'react-icons/fa';
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { id, name, job, image, text } = data[index];
+
+  const checkNumber = (number) => {
+    if (number > data.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return data.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((prevState) => {
+      const newIndex = prevState + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((prevState) => {
+      const newIndex = prevState - 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * data.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          Surprise me
+        </button>
+      </article>
+    </main>
+  );
 };
 export default App;
